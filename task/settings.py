00ee8e37b2decf27f3968908423a577a29f0d055
bartkoz <bartmkoz@gmail.com>
2025-01-28 22:41:37 +0100

    browsable api off

--- task/settings.py
+++ task/settings.py
@@ -114,6 +114,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-# REST_FRAMEWORK = {
-#     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
-# }
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+}
